307b43529d548dd8c1581579360a47c57ddc4cd9
Gabriel Prezzoti <gabrielbprezzoti@outlook.com>
2026-04-04 00:27:23 -0300

    add Start method and corresponding tests

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -31,6 +31,10 @@ type Campaign struct {
 	CreatedBy string    `validate:"email" gorm:"size:80"`
 }
 
+func (c *Campaign) Done() {
+	c.Status = Done
+}
+
 func (c *Campaign) Cancel() {
 	c.Status = Canceled
 }
