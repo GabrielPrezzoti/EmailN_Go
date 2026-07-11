1f48fbf795eee87ecb13d62341942edbf29dff8c
Gabriel Prezzoti <gabrielbprezzoti@outlook.com>
2026-07-10 21:22:48 -0300

    add goroutine for performance improve in service.go

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -12,6 +12,7 @@ const (
 	Started  string = "Started"
 	Done     string = "Done"
 	Canceled string = "Canceled"
+	Fail     string = "Failed"
 	Deleted  string = "Deleted"
 )
 
@@ -41,6 +42,14 @@ func (c *Campaign) Cancel() {
 
 func (c *Campaign) Delete() {
 	c.Status = Deleted
+}
+
+func (c *Campaign) Fail() {
+	c.Status = Fail
+}
+
+func (c *Campaign) Started() {
+	c.Status = Started
 }
 
 func NewCampaign(name string, content string, emails []string, createdBy string) (*Campaign, error) {
